2c49493f3c8e342e30fedbaa4a4b7cfba5812b46
Rob Kent <rob@gulon.co.uk>
2017-02-15 12:14:36 +0000

    Version bump

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ except OSError:
 
 setup(
     name="PushySDK",
-    version="0.1.2",
+    version="0.1.3",
     author="Rob Kent",
     author_email="jazzycamel@googlemail.com",
     description="A very simple Python client for the Pushy notification service API.",
